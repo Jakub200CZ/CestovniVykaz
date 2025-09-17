10abc1f2ca4ba00a82b3471fdf5f5ea2ae8baf35
Jakub Sedláček <kubka.sedlacek04@gmail.com>
2025-09-17 14:53:56 +0200

    Initial Commit

---
+++ CestovniVykaz/CestovniVykazApp.swift
@@ -0,0 +1,17 @@
+//
+//  CestovniVykazApp.swift
+//  CestovniVykaz
+//
+//  Created by Jakub Sedláček on 17.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct CestovniVykazApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CestovniVykazTests/CestovniVykazTests.swift
@@ -0,0 +1,17 @@
+//
+//  CestovniVykazTests.swift
+//  CestovniVykazTests
+//
+//  Created by Jakub Sedláček on 17.09.2025.
+//
+
+import Testing
+@testable import CestovniVykaz
+
+struct CestovniVykazTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
